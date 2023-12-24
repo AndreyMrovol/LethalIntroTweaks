7d7e3aa901d0e6e0b22ae85cd7b8ed8d9bd92cce
O.S <owen77stubbs@gmail.com>
2023-12-24 21:12:25 +0000

    MOAR LOGGING

--- Patches/MenuManagerPatch.cs
+++ Patches/MenuManagerPatch.cs
@@ -1,7 +1,9 @@
 using HarmonyLib;
 using System;
+
 using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
 using Object = UnityEngine.Object;
 
 namespace IntroTweaks.Patches {
@@ -23,17 +25,21 @@ namespace IntroTweaks.Patches {
         [HarmonyPatch("Awake")]
         static bool ReplaceVersionText(MenuManager __instance) {
             if (Plugin.Config.CUSTOM_VERSION_TEXT) {
-                GameObject original = __instance.versionNumberText.transform.gameObject;
-                GameObject clone = Object.Instantiate(original, __instance.menuButtons.transform);
-                original.SetActive(false);
-
-                clone.name = "VersionNumberText";
+                GameObject original = __instance.versionNumberText?.transform.gameObject;
+                if (!original) {
+                    Plugin.Logger.LogError("Reference to `versionNumberText` is null!");
+                }
 
                 try {
+                    GameObject clone = Object.Instantiate(original, __instance.menuButtons.transform);
+                    original.SetActive(false);
+
+                    clone.name = "VersionNumberText";
+
                     versionText = InitTextMesh(clone.GetComponent<TextMeshProUGUI>());
                     AnchorToBottom(clone.GetComponent<RectTransform>());
                 } catch(Exception e) {
-                    Plugin.Logger.LogError($"Error setting version text!\n{e}");
+                    Plugin.Logger.LogError($"Error creating custom version text!\n{e}");
                 }
             }
 
@@ -43,6 +49,21 @@ namespace IntroTweaks.Patches {
         [HarmonyPostfix]
         [HarmonyPatch("Start")]
         static void StartPatch(MenuManager __instance) {
+            try {
+                GameObject[] buttons = [
+                    GetButton(__instance.menuButtons, "HostButton"),
+                    __instance.lanButtonContainer,
+                    GetButton(__instance.menuButtons, "SettingsButton"),
+                    GetButton(__instance.menuButtons, "Credits"),
+                    GetButton(__instance.menuButtons, "QuitButton")
+                ];
+
+                FixPanelAlignment(__instance.menuButtons);
+                AlignButtons(buttons);
+            } catch(Exception e) {
+                Plugin.Logger.LogError(e);
+            }
+            
             if (Plugin.Config.REMOVE_NEWS_PANEL) {
                 __instance.NewsPanel.SetActive(false);
             }
@@ -98,6 +119,43 @@ namespace IntroTweaks.Patches {
                 Plugin.Config.VERSION_TEXT_Y, 
                 -37
             );
+        }
+
+        static void FixPanelAlignment(GameObject panel) {
+            // Make the white space equal on both sides of the panel.
+            var panelRect = panel.GetComponent<RectTransform>();
+ 
+            panelRect.anchoredPosition = Vector2.zero;
+            panelRect.anchoredPosition3D = Vector3.zero;
+
+            var panelPos = panelRect.position;
+            panelRect.position = new Vector3(panelPos.x + 2, panelPos.y, panelPos.z);
+        }
+
+        static void AlignButtons(GameObject[] buttons) {
+            // Align the messy menu buttons with each other.
+            foreach (GameObject button in buttons) {
+                var rect = button.GetComponent<RectTransform>();
+
+                rect.anchoredPosition = Vector2Int.FloorToInt(rect.anchoredPosition);
+                rect.anchoredPosition3D = Vector3Int.FloorToInt(rect.anchoredPosition3D);
+
+                Vector3 buttonPos = rect.position;
+                int newX = (int) Math.Floor(buttonPos.x);
+
+                rect.position = new Vector3(newX, buttonPos.y, buttonPos.z);
+            }
+
+            Plugin.Logger.LogDebug("Aligned menu buttons.");
+        }
+
+        static GameObject GetButton(GameObject panel, string name) {
+            try {
+                return panel.transform.Find(name).gameObject;
+            } catch(Exception e) { 
+                Plugin.Logger.LogError($"Error getting button: {name}\n{e}");
+                return null;
+            }
         }
     }
 }

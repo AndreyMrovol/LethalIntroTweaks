fee4b953218b941b1bb0c787441f9fb89379282c
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2024-02-16 12:52:15 +0100

    change plugin metadata for no config mismatches

--- Core/Metadata.cs
+++ Core/Metadata.cs
@@ -1,7 +1,8 @@
 namespace IntroTweaks.Core;
 
-static class Metadata {
-    public const string GUID = "io.github.IntroTweaks";
+static class Metadata
+{
+    public const string GUID = "mrov.IntroTweaks";
     public const string NAME = "IntroTweaks";
-    public const string VERSION = "1.4.6";
+    public const string VERSION = "1.4.7";
 }
